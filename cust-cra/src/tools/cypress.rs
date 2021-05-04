9f36ae7c7846ab0cd16fb5daa4de4c65d1fa6327
baremaximum <therealcharlesdesbiens@gmail.com>
2021-05-04 00:42:22 -0400

    moved package modifications.

--- cust-cra/src/tools/cypress.rs
+++ cust-cra/src/tools/cypress.rs
@@ -6,33 +6,8 @@ use std::{fs, process};
 pub fn install_cypress() {
     println!("{}", Cyan.paint("Installing Cypress..."));
 
-    // get package.json as an object
-    let mut package =
-        files::get_package().expect("an error occurred when attempting to open package.json");
-
-    // Get a mutable reference to the scripts object.
-    let scripts = package["scripts"]
-        .as_object_mut()
-        .expect("scripts field not found in json");
-    // Add cypress scripts
-    scripts.insert(
-        "dev".to_string(),
-        json!("concurrently -p \"[{name}]\" \"yarn run start*\""),
-    );
-    scripts.insert("start:cypress".to_string(), json!("cypress open"));
-
-    // get mutable reference to start script
-    let start = scripts["start"].as_str().unwrap().to_owned();
-
-    // insert cypress/instrument-cra into start script only if it isn't already there
-    if !start.contains("-r @cypress/instrument-cra start") {
-        let modified = start.replace("start", "-r @cypress/instrument-cra start");
-        scripts.insert("start".to_string(), json!(modified));
-    }
-
-    // save modified json.
-    files::save_package(package).expect("an error occured while trying to save");
-    println!("{}", Cyan.paint("package.json successfully modified!"));
+    // edit package.json as required
+    change_package();
 
     // run yarn commands
     let status = yarn::run_yarn_commands(vec![
@@ -62,6 +37,36 @@ pub fn install_cypress() {
     println!("{}", success_message);
 }
 
+fn change_package() {
+    // get package.json as an object
+    let mut package =
+        files::get_package().expect("an error occurred when attempting to open package.json");
+
+    // Get a mutable reference to the scripts object.
+    let scripts = package["scripts"]
+        .as_object_mut()
+        .expect("scripts field not found in json");
+    // Add cypress scripts
+    scripts.insert(
+        "dev".to_string(),
+        json!("concurrently -p \"[{name}]\" \"yarn run start*\""),
+    );
+    scripts.insert("start:cypress".to_string(), json!("cypress open"));
+
+    // get mutable reference to start script
+    let start = scripts["start"].as_str().unwrap().to_owned();
+
+    // insert cypress/instrument-cra into start script only if it isn't already there
+    if !start.contains("-r @cypress/instrument-cra start") {
+        let modified = start.replace("start", "-r @cypress/instrument-cra start");
+        scripts.insert("start".to_string(), json!(modified));
+    }
+
+    // save modified json.
+    files::save_package(package).expect("an error occured while trying to save");
+    println!("{}", Cyan.paint("package.json successfully modified!"));
+}
+
 const PLUGINS_INDEX: &str = r#"/// <reference types="cypress" />
 module.exports = (on, config) => {
     require('@cypress/code-coverage/task')(on, config)
@@ -81,7 +86,7 @@ mod tests {
     fn test_with_cypress_not_in_start() {
         test_utils::setup_json("react-scripts start");
 
-        install_cypress();
+        change_package();
         let package = files::get_package().unwrap();
 
         test_utils::cleanup();
